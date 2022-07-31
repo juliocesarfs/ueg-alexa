7bc2e92886de727c5e72aea07c63ef47bf154a10
julioalest <julio.silva@alest.com.br>
2022-07-31 10:21:19 -0300

    FODA-SE

---
+++ src/alexa/intents/index.ts
@@ -0,0 +1,14 @@
+import { SkillBuilders, DefaultApiClient } from 'ask-sdk-core';
+import { launchRequestHandler } from './launch/LaunchRequestHandler';
+
+const createSkill = () => {
+    const skillbuilder = SkillBuilders.custom();
+    return skillbuilder.addRequestHandlers(
+        launchRequestHandler,
+    )
+        .withApiClient(new DefaultApiClient())
+        .withCustomUserAgent('UEGbot/v1')
+        .create()
+}
+
+export { createSkill };
